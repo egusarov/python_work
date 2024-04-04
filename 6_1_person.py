621b6580401ffb1f42560b214480af534b7ea4a3
Evgeniy Gusarov <gusarov08@gmail.com>
2024-04-04 19:23:50 +0300

    Added homework: * 6_1_person.py * 6_2_favorite_numbers.py * 6_3_glossary.py

---
+++ 6_1_person.py
@@ -0,0 +1,11 @@
+person= {
+    'first name': 'kira',
+    'last name': 'gusarova',
+    'age': 9,
+    'city': 'odesa'
+    }
+
+print(f"First Name: {person['first name'].title()}")
+print(f"Last Name: {person['last name'].title()}")
+print(f"Age: {person['age']}")
+print(f"City: {person['city'].title()}")
