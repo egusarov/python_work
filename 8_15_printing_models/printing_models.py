cfb86c7144c2b416e54134b1e4a80cdb75e9200c
Evgeniy Gusarov <gusarov08@gmail.com>
2024-04-27 12:23:33 +0300

    Improvement

--- 8_15_printing_models/printing_models.py
+++ 8_15_printing_models/printing_models.py
@@ -3,5 +3,8 @@
 
 import printing_functions as pf
 
-pf.print_one()
-pf.print_two()
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+pf.print_models(unprinted_designs, completed_models)
+pf.show_completed_models(completed_models)
