33d99118eab22b4417fd956d146cb7bb92e030ac
Evgeniy Gusarov <gusarov08@gmail.com>
2024-05-06 17:03:04 +0300

    Added chapters to project

--- 7_7_infinity.py
+++
@@ -1,5 +0,0 @@
-# 7-7. Infinity: Write a loop that never ends, and run it. (To end the loop, press
-# CTRL-C or close the window displaying the output.)
-
-while True:
-    print("Infinity ...")
--- 8_16_imports/make_sandwich.py
+++
@@ -1,4 +0,0 @@
-def make_sandwich(*items):
-    print("\nMaking your sandwich with next item(s):")
-    for item in items:
-        print(f"- {item}")
