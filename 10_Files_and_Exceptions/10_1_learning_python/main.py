ef05c5b11520743eb5a1165cc252d95ab80b4511
Evgeniy Gusarov <gusarov08@gmail.com>
2024-05-07 16:30:26 +0300

    Added homework 10-1. Learning Python

---
+++ 10_Files_and_Exceptions/10_1_learning_python/main.py
@@ -0,0 +1,19 @@
+# 10-1. Learning Python: Open a blank file in your text editor and write a few lines
+# summarizing what you’ve learned about Python so far. Start each line with the
+# phrase In Python you can. . . . Save the file as learning_python.txt in the same
+# directory as your exercises from this chapter. Write a program that reads the file
+# and prints what you wrote two times: print the contents once by reading in the
+# entire file, and once by storing the lines in a list and then looping over each line.
+
+
+from pathlib import Path
+
+path = Path('/DEV/python_work/10_Files_and_Exceptions/10_1_learning_python/learning_python.txt')
+content = path.read_text()
+print("\nPrinting the contents once by reading in the entire file:")
+print(content)
+
+print("\nPrinting the contents by storing the lines in a list and then looping over each line:")
+lines = content.splitlines()
+for line in lines:
+    print(line)
