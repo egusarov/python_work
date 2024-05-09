02778cf3a6667fa292c84be921e6a9e93ae06c70
Evgeniy Gusarov <gusarov08@gmail.com>
2024-05-09 14:19:30 +0300

    10_6_addition/Improvement

--- 10_Files_and_Exceptions/10_6_addition/main.py
+++ 10_Files_and_Exceptions/10_6_addition/main.py
@@ -8,16 +8,20 @@ print("Give me two numbers, and I'll sum them.")
 print("Enter 'q' to quit.")
 
 while True:
-    number_1 = input("\nFirst number: ")
-    if number_1 == 'q':
-        break
-    number_2 = input("Second number: ")
-    if number_2 == 'q':
-        break
-    
-    try:
-        result = int(number_1) + int(number_2)
+    try: 
+        number_1 = input("\nFirst number: ")
+        if number_1 == 'q':
+             break
+        number_1 = int(number_1)
+        
+        number_2 = input("Second number: ")
+        if number_2 == 'q':
+            break     
+        number_2 = int(number_2)
+
     except ValueError:
-        print("Cannot sum the numbers. One of the input values is not a number. Please try again.")
+            print("Input value is not a number. Please try again.")
+
     else:
+        result = number_1 + number_2
         print(f"The sum of two numbers is {result}")
