7ece826bd95cabd01e44865ad8f8e92179ca63ce
Evgeniy Gusarov <gusarov08@gmail.com>
2024-05-21 16:28:33 +0300

    Alien Invasion: Controlling the Frame Rate

--- alien_invasion/alien _invasion.py
+++ alien_invasion/alien _invasion.py
@@ -8,20 +8,21 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
-    
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
-    
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             # Watch for keyboard and mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                   sys.exit()
-        
+                    sys.exit()
+
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+            self.clock.tick(60)
 
 
 if __name__ == '__main__':
