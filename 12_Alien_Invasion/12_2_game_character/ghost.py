198314eb3a40d30b09dafd9ee98f9608b6a7844b
Evgeniy Gusarov <gusarov08@gmail.com>
2024-05-24 16:34:11 +0300

    Added homework 12-4. Rocket

--- 12_Alien_Invasion/12_2_game_character/ghost.py
+++ 12_Alien_Invasion/12_2_game_character/ghost.py
@@ -10,7 +10,7 @@ class Ghost:
         self.screen_rect = gw_game.screen.get_rect()
 
         # Load the ghost image and get its rect
-        self.image = pygame.image.load('python_work/12_Alien Invasion/12_2_game_character/images/ghost.bmp')
+        self.image = pygame.image.load('images/ghost.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new ghost at the center of the screen
