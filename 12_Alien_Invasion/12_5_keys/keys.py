77e9e308e3a2ab833b6017f59427ed882bcb5b0a
Evgeniy Gusarov <gusarov08@gmail.com>
2024-05-28 15:29:36 +0300

    Added description for 12-5. Keys

--- 12_Alien_Invasion/12_5_keys/keys.py
+++ 12_Alien_Invasion/12_5_keys/keys.py
@@ -1,14 +1,13 @@
-import pygame
+import pygame, sys
 
 
 pygame.init()
 screen = pygame.display.set_mode((400, 240))
-running = True
 
-while running:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            sys.exit()
         elif event.type == pygame.KEYDOWN:
             print(event.key)
 
