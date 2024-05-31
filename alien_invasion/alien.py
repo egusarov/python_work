54d1a8db5e75ab087bd803716f0bd37fbca984e0
Evgeniy Gusarov <gusarov08@gmail.com>
2024-05-31 12:18:39 +0300

    Alien Invasion: Creating an Instance of the Alien

--- alien_invasion/alien.py
+++ alien_invasion/alien.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame.sprite import _Group, Sprite
+from pygame.sprite import Sprite
 
 
 class Alien(Sprite):
@@ -12,7 +12,7 @@ class Alien(Sprite):
 
         # Load the alien image and set its rect attribute
         self.image = pygame.image.load('images/alien.bmp')
-        self.rect = pygame.image.get_rect()
+        self.rect = self.image.get_rect()
 
         # Start each new alien near the top left of the screen
         self.rect.x = self.rect.width
