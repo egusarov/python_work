8b79a0b6cc5a4063fa9830f8d496b945686ba240
Evgeniy Gusarov <gusarov08@gmail.com>
2024-06-07 16:12:15 +0300

    Added homework 13-1. Stars

---
+++ 13_Aliens/13_1_stars/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A class to store all settings for the game."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 800
+        self.screen_height = 480
+        self.bg_color = (85, 170, 255)
