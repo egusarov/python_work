0da07ec159143cfc76da3899ae4c2f0026c33ef9
Evgeniy Gusarov <gusarov08@gmail.com>
2024-07-03 12:34:44 +0300

    Alien Invasion: Resetting the Score

--- alien_invasion/alien _invasion.py
+++ alien_invasion/alien _invasion.py
@@ -73,6 +73,7 @@ class AlienInvasion:
 
             # Reset the game statistics
             self.stats.reset_stats()
+            self.sb.prep_score()
             self.game_active = True
 
             # Get rid of any remaining bullets and alliens
